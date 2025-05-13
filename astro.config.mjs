1b012f75d3f393fc220d77513825e564dc922a00
Judas9016 <thetrollcrafttnt@gmail.com>
2025-05-13 02:59:18 -0500

    githubpages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  site: "https://github.com/Judas9016",
-  base: "portfolio",
+  site: "https://github.com/Judas9016/Judas9016.github.io",
 });

eaf3318e90166f1a63567e596243bde8043c90e6
Judas9016 <thetrollcrafttnt@gmail.com>
2025-05-13 02:45:01 -0500

    try to update to githubpages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  site: "https://github.com/Judas9016/Judas9016.github.io",
+  base: "mi-repo",
 });
